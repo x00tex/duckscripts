6eb201ae7d0ba8716373306496eda1f28c55aad9
poorduck <rj10110@yahoo.com>
2023-06-28 16:48:47 +0530

    update pureapkflow.py - add apkid support

--- pureapkflow.py
+++ pureapkflow.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Author :- @p00rduck (poorduck)
+Author :- @p00rduck (Poorduck)
 Date :- 2023-06-20
-Update :- 2023-06-23
+Update :- 2023-06-28 (apkID support added)
 Version: v0.0.2-Beta
-Description :- Download android apks from apkpure and and run through apileaks.
+Description :- Download android APK(s) from APKPure and and run through APKLeaks.
 
 Requirements :-
 
@@ -14,12 +14,17 @@ Requirements :-
     rich==13.4.1
     apkleaks==2.6.1
     python-magic==0.4.27
+    apkid==2.1.1
 
 Known error and possible fixes :-
 
 - ImportError: cannot import name 'DEFAULT_CIPHERS' from 'urllib3.util.ssl_'
 
     pip install 'urllib3<2'
+
+- yara.Error: internal error: 34
+
+    pip install -U git+https://github.com/MobSF/yara-python-dex
 
 Quality Tweaks :-
 
@@ -31,7 +36,6 @@ Quality Tweaks :-
 
 """
 
-
 import argparse
 import os
 from bs4 import BeautifulSoup
@@ -40,7 +44,7 @@ from rich.console import Console
 import concurrent.futures
 import cfscrape  # https://github.com/Anorov/cloudflare-scrape
 import argparse
-from apkleaks.apkleaks import APKLeaks
+from apkleaks.apkleaks import APKLeaks  # https://github.com/dwisiswant0/apkleaks
 import magic
 import zipfile
 import tempfile
@@ -49,6 +53,8 @@ from io import StringIO
 import requests
 import json
 import re
+from apkid.apkid import Scanner, Options  # https://github.com/rednaga/APKiD
+from contextlib import redirect_stdout
 
 
 class APKLeaksRunner:
@@ -66,7 +72,7 @@ class APKLeaksRunner:
             dummy_stream = StringIO()  # Create a dummy file-like object to redirect stdout and stderr
             # Redirect to the dummy object
             sys.stdout = dummy_stream
-            sys.stderr = dummy_stream
+            # sys.stderr = dummy_stream
 
             self.init.integrity()
             self.init.decompile()
@@ -76,10 +82,14 @@ class APKLeaksRunner:
 
             # Restore to its original value
             sys.stdout = sys.__stdout__
-            sys.stderr = sys.__stderr__
+            # sys.stderr = sys.__stderr__
 
     def apkleaks(self, apk, ftype):
         output_file = apk + "-apkleaks.txt"
+
+        if os.path.exists(output_file):
+            console.log("... [yellow]APKLeaks[/yellow] :- [bold yellow]skipping[/bold yellow]")
+            return
 
         if "Zip archive data" in ftype:
             with tempfile.TemporaryDirectory() as temp_dir:
@@ -117,12 +127,58 @@ class APKLeaksRunner:
             try:
                 self._run(inputarg=apk, outputarg=output_file)
             except Exception as e:
-                pass
+                return False
+
+
+class APKiDRunner:
+    def __init__(self):
+        self.options = None
+        self.scanner = None
+
+    # TODO: Add arguments support direct in the script inputs.
+    def build_options(self):
+        return Options(
+            timeout=30,  # Yara scan timeout (in seconds)
+            verbose=False,  # log debug messages
+            json=False,  # write individual results here (implies --json)
+            output_dir=None,  # output scan results in JSON format
+            typing="magic",  # method to decide which files to scan, choices = ('magic', 'filename', 'none')
+            entry_max_scan_size=100 * 1024 * 1024,  # max zip entry size to scan in bytes, 0 = no limit
+            scan_depth=2,  # how deep to go when scanning nested zips, 2 is enought for xapk
+            recursive=False,  # recurse into subdirectories
+            include_types=False  # include file type info for matched files
+        )
+
+    def run(self, apkfile):
+
+        global console
+
+        try:
+            original_stdout = sys.stdout  # Store the original sys.stdout in a variable
+            dummy_stream = StringIO()  # Create a dummy file-like object to redirect stdout and stderr
+
+            with redirect_stdout(dummy_stream):
+                self.options = self.build_options()
+                rules = self.options.rules_manager.load()
+                self.scanner = Scanner(rules, self.options)
+
+                self.scanner.scan(apkfile)
+
+            # Get the content from the dummy_stream
+            output = dummy_stream.getvalue()
+
+            lines = output.split('\n')
+            for line in lines:
+                console.log(line)
+
+        finally:
+            sys.stdout = original_stdout  # Restore sys.stdout to its original value
+            # sys.stderr = sys.__stderr__
 
 
 def apkpureWebScrapper(url, package_name):
 
-    global webScrapper, console, downloadLinksJSON
+    global _WEB_SCRAPPER, console, downloadLinksJSON
 
     package_uri = ""
 
@@ -132,7 +188,7 @@ def apkpureWebScrapper(url, package_name):
 
     while retry_count < max_retries:
         try:
-            response = webScrapper.get(f"{url}/search?q=" + package_name)
+            response = _WEB_SCRAPPER.get(f"{url}/search?q=" + package_name)
 
             if "Cloudflare Ray ID" in response.text:
                 console.log("[yellow]Cloudflare protection could not be bypassed, trying again..[/yellow]")
@@ -171,7 +227,7 @@ def apkpureWebScrapper(url, package_name):
 
     # Extract all versions download URLs
     print("[+] Extracting download links, please wait...")
-    response = webScrapper.get(url + package_uri + "/versions").text
+    response = _WEB_SCRAPPER.get(url + package_uri + "/versions").text
     soup = BeautifulSoup(response, "html.parser")
 
     ver_elements_div = soup.find("ul", class_="ver-wrap")
@@ -182,18 +238,19 @@ def apkpureWebScrapper(url, package_name):
         for href_link in ver_download_link:
                 
             if href_link is not None:
-                download_page = webScrapper.get(href_link).text
+                download_page = _WEB_SCRAPPER.get(href_link).text
                 if "Download Variant" in download_page:
                     console.log("[yellow]Found multiple variants for " + href_link.split("/")[-1] + "[/yellow]")
                     soup = BeautifulSoup(download_page, "html.parser")
                     variants = soup.findAll("div", class_="table-cell down")
                     variants_uris = [div.find('a')['href'] for div in variants]
                     for variant in variants_uris:
-                        variant_download_page = webScrapper.get(url + variant).text
+                        variant_download_page = _WEB_SCRAPPER.get(url + variant).text
                         soup = BeautifulSoup(variant_download_page, "html.parser")
                         download_btn_element = soup.find("a", class_="download-start-btn")
                         variant_download_url = download_btn_element['href']
-                        if not any(entry["download_url"] == variant_download_url for entry in applications): # In some cases, it appears that the apkpure provides the same APK file for all variants (?)
+                        # In some cases, it appears that the apkpure provides the same APK file for all variants (?)
+                        if not any(entry["download_url"] == variant_download_url for entry in applications):
                             veriant_version = soup.find('span', class_='info-sdk').find('span').text
                             veriant_name = variant_download_url.split("/")[-1].replace("?", "-").replace("=", "_")
                             app_name = re.sub(r'^(.*?)(-versionCode_\d+)$', fr'\g<1>-{veriant_version}\g<2>', veriant_name)
@@ -203,11 +260,12 @@ def apkpureWebScrapper(url, package_name):
                                 app_name = app_name + ".apk"
 
                             applications.append({'filename': app_name, 'download_url': variant_download_url})
-                            # sleep(5)  # Keep it slow
+                            # wait(5)  # Keep it slow
                 else:
                     soup = BeautifulSoup(download_page, "html.parser")
                     download_btn_element = soup.find("a", class_="download-start-btn")
                     download_url = download_btn_element['href']
+                    # In some cases, it appears that the apkpure provides the same APK file for all variants (?)
                     if not any(entry["download_url"] == download_url for entry in applications):
                         version = soup.find('span', class_='info-sdk').find('span').text
                         name = download_url.split("/")[-1].replace("?", "-").replace("=", "_")
@@ -218,7 +276,7 @@ def apkpureWebScrapper(url, package_name):
                             app_name = app_name + ".apk"
 
                         applications.append({'filename': app_name, 'download_url': download_url})
-                        # sleep(5)  # Keep it slow
+                        # wait(5)  # Keep it slow
 
     except KeyboardInterrupt as e:
         exit(e)
@@ -233,7 +291,7 @@ def apkpureWebScrapper(url, package_name):
 
 def apkpureAPIScrapper(url, package_name):
 
-    global console, apiScrapper, downloadLinksJSON
+    global _API_SCRAPPER, console, downloadLinksJSON
 
     headers = {
     "User-Agent-Webview": "Mozilla/5.0 (Linux; Android 8.1.0; Pixel Build/OPM6.171019.030.E1; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.186 Mobile Safari/537.36",
@@ -245,9 +303,9 @@ def apkpureAPIScrapper(url, package_name):
     uri = "/v3/get_app_his_version"
     param = {"package_name": package_name}
 
-    apiScrapper.headers = headers
+    _API_SCRAPPER.headers = headers
 
-    data = apiScrapper.get(url + uri, params=param)
+    data = _API_SCRAPPER.get(url + uri, params=param)
     version_list = data.json()["version_list"]
     applications = []
 
@@ -283,26 +341,29 @@ def main():
 
     # Parse required argument(s) for the script.
     parser = argparse.ArgumentParser(description='Download all versions of an Android mobile application from apkpure.com')
+    parser.add_argument('-nd', action='store_false', default=True, help="Disable downloading, only extract download links.")
+    parser.add_argument('--apkid', action='store_true', default=False, help="Run apkID on every app.")
+
     required = parser.add_argument_group('required arguments')
     required.add_argument('-p', required=True, metavar="packagename", help="example: com.twitter.android")
-    parser.add_argument('-nd', action='store_false', default=True, help="Disable downloading, only extract download links.")
-
+    
     subparsers = parser.add_subparsers(dest='tool', metavar='OPTION', help='Choose the analysis tool ["apkleaks"]')
 
     apkleaks_parser = subparsers.add_parser('apkleaks', help='Run APKLeaks tool')
     apkleaks_parser.add_argument("-f", "--file", type=str, required=False, help=argparse.SUPPRESS)
     apkleaks_parser.add_argument("-o", "--output", type=str, required=False, help=argparse.SUPPRESS)
     apkleaks_parser.add_argument("-p", "--pattern", help="Path to custom patterns JSON", type=str, required=False)
-    apkleaks_parser.add_argument("-a", "--args", help="Disassembler arguments (e.g. \"--threads-count 5 --deobf\")", type=str, required=False)
+    apkleaks_parser.add_argument("-a", "--args", help="Disassembler arguments (e.g. \"--threads-count 5 --deobf\"), (default threads-count set to 1)",
+                                 type=str, required=False, default="--threads-count 1")
     apkleaks_parser.add_argument("--json", help="Save as JSON format", required=False, action="store_true")
 
     args = parser.parse_args()
 
-    global webScrapper, console, apiScrapper, downloadLinksJSON, packageName
+    global _API_SCRAPPER, _WEB_SCRAPPER, console, downloadLinksJSON
     
-    webScrapper = cfscrape.create_scraper(delay=10)
-    apiScrapper = requests.session()
-    console = Console()
+    _API_SCRAPPER = requests.session()
+    _WEB_SCRAPPER = cfscrape.create_scraper(delay=10)
+    console = Console(log_path=False) # log_time_format="%H:%M:%S.%f"
     packageName = args.p
     downloadLinksJSON = packageName + "-downloadlinks.json"
 
@@ -319,7 +380,6 @@ def main():
         console.print("[yellow][!] Download links file already exists.[/yellow]")
         with open(downloadLinksJSON, 'r') as fr:
             bigData = json.loads(fr.read())
-
     else:
         print("[+] Scrapping download links.")
         with console.status("[bold green]Scrapping...") as status:
@@ -331,7 +391,8 @@ def main():
                 scrapper = "API"
             except requests.exceptions.RequestException:  # Fallback to Web 
                 try:
-                    response = requests.get(web_url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0"})
+                    response = requests.get(web_url, headers={
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0"})
                     response.raise_for_status()
                     url = web_url
                     scrapper = "WEB"
@@ -349,10 +410,9 @@ def main():
                 console.print("[bold red][-] No Scrapper Found![/bold red]")
                 exit(1)
 
-
     def _download_apk(url, filename):
 
-        print("[+] Working on :-", filename)
+        console.print("[+] Working on :-", "[yellow]"+filename+"[/yellow]")
 
         current_directory = os.getcwd()
         final_directory = os.path.join(current_directory, packageName)
@@ -363,13 +423,18 @@ def main():
         absoluteFile = os.path.join(final_directory, filename)
 
         if os.path.exists(absoluteFile):
-            console.print("[yellow]... " + filename + " is already exists.[/yellow]")
+            console.log("... " + filename + " [yellow] Already Exist.[/yellow]")
         else:
             with console.status("[bold green]Downloading...") as status:
-                file = webScrapper.get(url)
+                file = requests.get(url)
                 open(absoluteFile, "wb").write(file.content)
 
         file_type = magic.from_file(absoluteFile)
+
+        if args.apkid:
+            console.log("... [yellow]ApkID[/yellow] :-", filename)
+            apkidrunner = APKiDRunner()
+            apkidrunner.run(apkfile=absoluteFile)
 
         if args.tool == 'apkleaks':
             with console.status("[bold green]Running apkleaks...") as status:
@@ -393,14 +458,17 @@ def main():
                 print("[!] Nothing new!")
 
         except KeyboardInterrupt:
-            print("\n[!] Keyboard interruption detected while downloading, we'll shutdown execution after running threads are finished.")
-            wait(2)
-            print("... Promise!")
-            executor.shutdown(cancel_futures=True)
-        except Exception as e:
-            exit(e)
+            try:
+                console.print("[yellow][!] Keyboard interruption detected. We'll shutdown execution after running threads are finished.[/yellow]")
+                wait(1)
+                print("... Promise!")
+                executor.shutdown(cancel_futures=True)
+            except KeyboardInterrupt as e:
+                console.print("\n[red][!] Keyboard interruption detected again. Exiting immediately...[/red]")
+                os._exit(0)  # Forcefully terminate the process without raising further exceptions
 
     console.print("[bold green][+] Done![/bold green]")
+
 
 if __name__ == "__main__":
 
